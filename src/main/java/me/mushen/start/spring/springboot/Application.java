40a59dba833e71fb518ac0428432770cc1ffc733
wangchangwei <wangchangwei@meituan.com>
2018-07-08 16:41:01 +0800

    init project

---
+++ src/main/java/me/mushen/start/spring/springboot/Application.java
@@ -0,0 +1,12 @@
+package me.mushen.start.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
